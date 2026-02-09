e3991f110cd56d0d939fe02824cba0eb46d1236b
ErikCaballeroh <erikaballero.hdz@gmail.com>
2026-02-08 23:51:30 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View className='mt-10'>
+            <Text className='text-4xl font-bold text-blue-800 text-center'>Note2Quiz</Text>
+        </View>
+    )
+}
+
+export default Home
